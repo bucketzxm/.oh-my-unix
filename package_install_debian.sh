20ce08ab2f4a6dcbfd1435dd39bc47a22086bd0f
simon zheng <simon.zheng@mujin.co.jp>
2018-06-27 16:04:26 +0900

    add check for link

--- package_install_debian.sh
+++ package_install_debian.sh
@@ -23,26 +23,58 @@ else
     mkdir $CONFIG_BACKUP_PATH
 fi
 
+function check_link(){
+    my_link=$1
+    if [ -L ${my_link} ] ; then
+        if [ -e ${my_link} ] ; then
+            echo "Good link"
+            return 1
+        else
+            echo "Broken link"
+        fi
+    elif [ -e ${my_link} ] ; then
+        echo "Not a link"
+        return 0
+    else
+        return 0
+    fi
+}
 
+
+CONFIG_FILES=""
 if [ -f ~/.bashrc ]; then
     mv ~/.bashrc $CONFIG_BACKUP_PATH/.bashrc
 fi
-ln -s .bashrc ~/.bashrc
+
+if [ ! check_link ~/.bashrc ]; then
+    ln -s .bashrc ~/.bashrc
+fi
+
 
 if [ -f ~/.bash_aliases ]; then
     mv ~/.bash_aliases $CONFIG_BACKUP_PATH/.bash_aliases
 fi
-ln -s .bash_aliases ~/.bash_aliases
+
+if [ ! check_link ~/.bash_aliases ]; then
+    ln -s .bash_aliases ~/.bash_aliases
+fi
 
 if [ -f ~/.bash_profile ]; then
    mv ~/.bash_profile $CONFIG_BACKUP_PATH/.bash_profile
 fi
-ln -s .bash_profile ~/.bash_aliases
+if [ ! check_link ~/.bash_profile ]; then
+    ln -s .bash_profile ~/.bash_aliases
+fi
+
 
 if [ -f ~/.zshrc ]; then
     mv ~/.zshrc $CONFIG_BACKUP_PATH/.zshrc
 fi
-ln -s .zshrc ~/.zshrc
+
+if [ ! check_link ~/.zshrc ]; then
+    ln -s .zshrc ~/.zshrc
+fi
+
 
 
 
