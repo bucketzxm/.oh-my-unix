c3e73c242a0679cb554c8db1fbfa8cbe7b9b6f2a
simon zheng <simon.zheng@mujin.co.jp>
2018-06-27 10:53:37 +0900

    compile cquery

--- package_install_debian.sh
+++ package_install_debian.sh
@@ -1,6 +1,15 @@
+sudo apt-get update && apt-get upgrade
 git submodule update --init
 
 # compile cquery
-cd 3rdparty
+cd 3rdparty/cquery
+git submodule update --init && ./waf configure build # --variant=debug if you want to report issure
+export PATH=$PATH:`pwd`/build/release/bin/
+
+
+# install rust if you want
+# curl https://sh.rustup.rs -sSf | sh
+
+
 
 
