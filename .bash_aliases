2bb624212c8f480e4c27f8665a969c33d34ddbfd
mujin <mujin@debian.mujin.co.jp>
2019-03-19 09:47:48 +0900

    add docker install and flake8 commands

--- .bash_aliases
+++ .bash_aliases
@@ -9,3 +9,4 @@ alias diff='colordiff'
 
 alias nuget="mono /usr/local/bin/nuget.exe"
 alias flake8="flake8 --show-source --ignore=E251,E261,E301,E302,E303,E305,E501"
+alias emacs27="env HOME=~/.emacs.d.bucketzxm emacs-27.0.50"
--- package_install_debian.sh
+++ package_install_debian.sh
@@ -1,6 +1,5 @@
 set -e
 
-
 SERVER_MODE=$1
 
 sudo apt-get update && sudo apt-get upgrade
@@ -47,6 +46,27 @@ fi
 
 # choose terminal
 sudo update-alternatives --config x-terminal-emulator
+
+
+# install docker
+# uninstall old version first
+sudo apt-get remove docker docker-engine docker.io containerd runc
+sudo apt-get update
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg2 \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/debian \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+
 
 
 # emacs elpy 
